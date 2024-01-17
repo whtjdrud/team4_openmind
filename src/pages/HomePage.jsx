3a7b2bce655535e89efbbc32918d6d547d9c925a
LeeYounSung <chance07236@naver.com>
2024-01-17 19:13:22 +0900

    [UPDATE] 버튼 컴포넌트 제작

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { Button } from '../components/atomicComponents/Button'
 
 const HomePage = () => {
   return (
     <div>
       <h1>홈페이지</h1>
+      <Button>추가하기</Button>
     </div>
   )
 }
