4213e5014463746c2d19a651dc0f60a78a3afd75
Jo hyeonjin <guswlso2@naver.com>
2024-01-17 16:55:58 +0900

    CSS

---
+++ src/Theme.js
@@ -0,0 +1,35 @@
+const theme = {
+  colors: {
+    Grayscale10: '#FFF',
+    Grayscale20: '#F9F9F9',
+    Grayscale30: '#CFCFCF',
+    Grayscale40: '#818181',
+    Grayscale50: '#515151',
+    Grayscale60: '#000',
+
+    Brown10: '#F5F1EE',
+    Brown20: '#E4D5C9',
+    Brown30: '#C7BBB5',
+    Brown40: '#542F1A',
+    Brown50: '#341909',
+
+    Blue50: '#1877F2',
+    Yellow50: '#FEE500',
+    Red50: '#B93333',
+
+    // 다른 변수들도 필요에 따라 추가할 수 있습니다.
+    // 예: Grayscale30, Grayscale40 등
+  },
+  fonts: {
+    body: "'Roboto', sans-serif",
+    heading: "'Roboto', sans-serif",
+  },
+  shadows: {
+    small: '0px 4px 4px 0px rgba(140, 140, 140, 0.25)',
+    medium: '0px 4px 4px 0px rgba(0, 0, 0, 0.25)',
+    large: '0px 16px 20px 0px rgba(48, 48, 48, 0.62)',
+    // 필요에 따라 다양한 쉐도우를 추가할 수 있습니다.
+  },
+}
+
+export default theme
