3cfb875bfa2057f8b12cb3d3ca79374a054e3931
jiyong <parkjiy96@naver.com>
2024-01-18 20:54:08 +0900

    feat : ShareBtn 컴포넌트 및 스타일  추가 / whitelink, whitefacebook 로고 추가

--- src/components/atomicComponents/Share/index.jsx
+++ src/components/atomicComponents/Share/index.jsx
@@ -1,4 +1,17 @@
-const Share = () => {
-  return <div></div>
+import React from 'react'
+import { ShareBtnDiv, ShareLinkImg, ShareFaceBookImg, ShareKakaoImg } from './StyleShareBtn'
+import LinkSvg from '../../../assets/images/whitelink.svg'
+import FacebookSvg from '../../../assets/images/whitefacebook.svg'
+import KakaotalkSvg from '../../../assets/images/Kakaotalk.svg'
+
+const ShareBtn = () => {
+  return (
+    <ShareBtnDiv>
+      <ShareLinkImg src={LinkSvg} alt='link' />
+      <ShareFaceBookImg src={FacebookSvg} alt='facebook' />
+      <ShareKakaoImg src={KakaotalkSvg} alt='kakao' />
+    </ShareBtnDiv>
+  )
 }
-export default Share
+
+export default ShareBtn
