d0e6917946173e4bb11d87854f743ed90b4ea6c8
LeeYounSung <chance07236@naver.com>
2024-01-18 16:44:53 +0900

    [FIX] develop 브랜치 최신화

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
-import { Button } from '../components/atomicComponents/Button'
 
 const HomePage = () => {
   return (
     <div>
       <h1>홈페이지</h1>
-      <Button>추가하기</Button>
     </div>
   )
 }
