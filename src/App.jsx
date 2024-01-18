42cfc25c7123e5468f676087d51dbc93fe338d12
jiyong <parkjiy96@naver.com>
2024-01-18 15:10:06 +0900

    feat : 글로벌 스타일 pull

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,23 @@ import AnswerPage from './pages/AnswerPage'
 import IndividualFeed from './pages/IndividualFeed'
 import HomePage from './pages/HomePage'
 import QuestionList from './pages/QuestionList'
+import InputField from './components/atomicComponents/inputField'
+import GlobalStyle from './style/GlobalStyle'
 
 const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path='/post/:id/answer' element={<AnswerPage />} />
-        <Route path='/post/:id' element={<IndividualFeed />} />
-        <Route path='/list' element={<QuestionList />} />
-        <Route path='/' element={<HomePage />} />
-      </Routes>
-    </Router>
+    <>
+      <GlobalStyle />
+      <Router>
+        <Routes>
+          <Route path='/post/:id/answer' element={<AnswerPage />} />
+          <Route path='/post/:id' element={<IndividualFeed />} />
+          <Route path='/list' element={<QuestionList />} />
+          <Route path='/' element={<HomePage />} />
+          <Route path='/test' element={<InputField />} />
+        </Routes>
+      </Router>
+    </>
   )
 }
 
