b9c1d9e5a5d183856bb0e0c2522d70aa38531d1e
LeeYounSung <chance07236@naver.com>
2024-01-24 17:20:59 +0900

    Pull을 위한 중간 커밋 답변하러 가기 버튼 작업중

--- src/components/moduleComponents/ListHeader/index.jsx
+++ src/components/moduleComponents/ListHeader/index.jsx
@@ -7,16 +7,17 @@ import { Link } from 'react-router-dom'
 const ListHeader = () => {
   const [localId, setLocalId] = useState('')
   const handleClick = () => {
-    setLocalId(localStorage.getItem()) // 로컬스토리지에서 id를  가져오기 // onClick, Link 의 실행순서가 궁금
+    setLocalId(localStorage.getItem('userId')) // 로컬스토리지에서 id를  가져오기 // onClick, Link 의 실행순서가 궁금
+    console.log(localId)
   }
   return (
     <StyledListHeader>
       <Link to='/'>
         <img className='logo-img' src={logo} alt='로고' />
       </Link>
-      <Link to={`${localId}` ? `/post/${localId}/answer` : '/'}>
-        <ReplyButton onClick={handleClick}>답변하러 가기</ReplyButton>
-      </Link>
+      {/* <Link to={`${localId}` ? `/post/${localId}/answer` : '/'}>
+      </Link> */}
+      <ReplyButton onClick={handleClick}>답변하러 가기</ReplyButton>
     </StyledListHeader>
   )
 }
