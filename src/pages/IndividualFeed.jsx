2f214ace78d374ead0cdd20fc8802261c9459eaa
LeeYounSung <chance07236@naver.com>
2024-01-24 19:34:47 +0900

    [fix] AnswerPage & IndividualFeed 컴포넌트가 서로 바뀌어서 교체해줌

--- src/pages/IndividualFeed.jsx
+++ src/pages/IndividualFeed.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
-import { AnswerPageComponent } from '../components/moduleComponents/AnswerPageComponents'
+import { AskPageComponent } from '../components/moduleComponents/AskPageComponents'
 
 const IndividualFeed = () => {
   const { id } = useParams()
   // 이제 id변수에 url에 적혀있는 id숫자가 담겨있습니다
-  return <AnswerPageComponent id={id} />
+  return <AskPageComponent id={id} />
 }
 
 export default IndividualFeed
