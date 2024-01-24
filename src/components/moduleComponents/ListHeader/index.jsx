4364dea944d715f345d09adeb45ff88ebe5d1070
LeeYounSung <chance07236@naver.com>
2024-01-24 17:37:16 +0900

    [fix]: feedCard 텍스트 영역 오류 수정

--- src/components/moduleComponents/ListHeader/index.jsx
+++ src/components/moduleComponents/ListHeader/index.jsx
@@ -7,8 +7,8 @@ import { Link } from 'react-router-dom'
 const ListHeader = () => {
   const [localId, setLocalId] = useState('')
   const handleClick = () => {
-    setLocalId(localStorage.getItem('userId')) // 로컬스토리지에서 id를  가져오기 // onClick, Link 의 실행순서가 궁금
-    console.log(localId)
+    const userId = localStorage.getItem('userId') // 로컬스토리지에서 id를  가져오기 // onClick, Link 의 실행순서가 궁금
+    console.log(userId)
   }
   return (
     <StyledListHeader>
