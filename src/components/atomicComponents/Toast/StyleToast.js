c8fb71211dfcb3e77ba38a8d06740543e63388c5
jiyong <parkjiy96@naver.com>
2024-01-25 02:20:58 +0900

    refactor : toast 텍스트 애니메이션 추가

--- src/components/atomicComponents/Toast/StyleToast.js
+++ src/components/atomicComponents/Toast/StyleToast.js
@@ -1,7 +1,14 @@
 import styled from 'styled-components'
 
-export const ToastDiv = styled.div`
+export const ToastBox = styled.div`
+  position: relative;
+`
+
+export const ToastDiv = styled.span`
   width: 167px;
+  position: absolute;
+  top: 0.5rem;
+  left: -8rem;
   padding: 12px 20px;
   background-color: var(--Grayscale-60, #000);
   color: var(--Grayscale-10, #fff);
@@ -12,9 +19,7 @@ export const ToastDiv = styled.div`
   font-weight: 500;
   font-family: Pretendard;
   // 여기서부터 추가한거
-  margin-top: 10px;
-  animation: fadeout 3s;
-  -webkit-animation: fadeout 5s;
+  animation: fadeout 5s;
   animation-fill-mode: forwards;
   @keyframes fadeout {
     from {
@@ -25,3 +30,28 @@ export const ToastDiv = styled.div`
     }
   }
 `
+// export const ToastDiv = styled.div`
+//   width: 167px;
+//   padding: 12px 20px;
+//   background-color: var(--Grayscale-60, #000);
+//   color: var(--Grayscale-10, #fff);
+//   border-radius: 8px;
+//   box-shadow: var(--shadow-medium, 0px 4px 4px 0px rgba(0, 0, 0, 0.25));
+//   text-align: center;
+//   font-size: 14px;
+//   font-weight: 500;
+//   font-family: Pretendard;
+//   // 여기서부터 추가한거
+//   margin-top: 10px;
+//   animation: fadeout 3s;
+//   -webkit-animation: fadeout 5s;
+//   animation-fill-mode: forwards;
+//   @keyframes fadeout {
+//     from {
+//       opacity: 1;
+//     }
+//     to {
+//       opacity: 0;
+//     }
+//   }
+// `
--- src/components/atomicComponents/Toast/index.jsx
+++ src/components/atomicComponents/Toast/index.jsx
@@ -1,7 +1,11 @@
-import { ToastDiv } from './StyleToast'
+import { ToastDiv, ToastBox } from './StyleToast'
 
 const Toast = () => {
-  return <ToastDiv>URL이 복사되었습니다</ToastDiv>
+  return (
+    <ToastBox>
+      <ToastDiv>URL이 복사되었습니다</ToastDiv>
+    </ToastBox>
+  )
 }
 
 export default Toast
