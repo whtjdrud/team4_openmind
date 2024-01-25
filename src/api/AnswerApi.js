8b9cb8b15b35151a330b5065e8c1f5108f8a163d
whtjdrud <151587265+whtjdrud@users.noreply.github.com>
2024-01-25 13:58:14 +0900

    refactor : 응답페이지 리팩토링 - Header 분리 - 데이터 처리 부분 hook 분리

---
+++ src/api/AnswerApi.js
@@ -0,0 +1,16 @@
+const API_BASE_URL = 'https://openmind-api.vercel.app/3-4/subjects/'
+export const getSubject = async (id) => {
+  try {
+    const response = await fetch(`${API_BASE_URL}${id}/`)
+    if (!response.ok) {
+      throw new Error('Network response was not ok')
+    }
+    return response.json()
+  } catch (err) {
+    return null
+  }
+}
+export const fetchQuestions = async (id) => {
+  const response = await fetch(`${API_BASE_URL}${id}/questions/`)
+  return response.json()
+}
--- src/components/atomicComponents/FeedCard/index.jsx
+++ src/components/atomicComponents/FeedCard/index.jsx
@@ -5,55 +5,46 @@ import QuestionComponent from './Question'
 import ReplyComponent from './Reply'
 import ButtonsComponent from './Buttons'
 import FeedCardEmpty from '../FeedCardEmpty'
+import { fetchQuestions } from '../../../api/AnswerApi'
 
 const FeedCard = ({ question, id, like, dislike, answer, isAskPage }) => {
-  const [isAnswered, setIsAnswered] = useState(false)
   return (
     <CardLayout>
-      <IconsComponent isAnswered={isAnswered} answerId={answer && answer.id} />
+      <IconsComponent isAnswered={!!answer} answerId={answer?.id} />
       <QuestionComponent question={question} />
       {isAskPage && answer && <ReplyComponent answer={answer.content} />}
       {isAskPage || (answer ? <ReplyComponent answer={answer.content} /> : <FeedCardEmpty questionId={id} />)}
       <ButtonsComponent like={like} dislike={dislike} />
+      {isAskPage && answer && <ReplyComponent answer={answer.content} />}
     </CardLayout>
   )
 }
 
 const FeedCards = ({ id, isAskPage }) => {
   const [feeds, setFeeds] = useState([])
-  const API_BASE_URL = 'https://openmind-api.vercel.app/3-4/subjects/'
-
-  const getQuestions = async (subjectId) => {
-    const question = await fetch(`${API_BASE_URL}${id ? `${subjectId}/questions/` : ''}`)
-    return question.json()
+  const fetchAndSetQuestions = async () => {
+    const questionsData = await fetchQuestions(id)
+    setFeeds(questionsData.results)
   }
-
-  const getQuestionFnc = async (value) => {
-    const { results } = await getQuestions(value)
-    console.log(getQuestions(value))
-    // console.log(results)
-    setFeeds(results)
-  }
-
   useEffect(() => {
-    getQuestionFnc(id)
-  }, [])
+    fetchAndSetQuestions()
+  }, [id])
 
-  return feeds.map((feed) => (
-    <FeedCard
-      key={feed.id}
-      id={feed.id}
-      answer={feed.answer}
-      question={feed.content}
-      like={feed.like}
-      dislike={feed.dislike}
-      isAskPage={isAskPage}
-    />
-  ))
-}
-
-FeedCards.defaultProps = {
-  isAskPage: false,
+  return (
+    <>
+      {feeds.map((feed) => (
+        <FeedCard
+          key={feed.id}
+          id={feed.id}
+          answer={feed.answer}
+          question={feed.content}
+          like={feed.like}
+          dislike={feed.dislike}
+          isAskPage={isAskPage}
+        />
+      ))}
+    </>
+  )
 }
 
 export default FeedCards
