ebcec04f22db9fe3bf0391a17bf071f6b05ec300
jiyong <parkjiy96@naver.com>
2024-01-26 21:29:08 +0900

    feat : 홈페이지 타블렛 반응형 구현

--- src/components/moduleComponents/LoginToggleLogic/HomePageStyle.js
+++ src/components/moduleComponents/LoginToggleLogic/HomePageStyle.js
@@ -2,6 +2,10 @@ import styled from 'styled-components'
 import HomeBackImg from '../../../assets/images/HomeBackImg.png'
 import { Link } from 'react-router-dom'
 
+// Pc : 1200px 이상
+// Tablet : 768px~1199px
+// Mobile : 375px~767p
+
 export const MainPageDiv = styled.div`
   width: 100%;
   height: 100vh; // viewport 높이로 설정
@@ -9,6 +13,12 @@ export const MainPageDiv = styled.div`
   background-size: cover;
   background-repeat: no-repeat;
   background-position: center;
+
+  @media only screen and (max-width: 1199px) {
+    background-image: none;
+    background-color: var(--Grayscale-20, #f9f9f9);
+  }
+
   @media only screen and (max-width: 767px) {
     background-image: none;
     background-color: var(--Grayscale-20, #f9f9f9);
@@ -22,6 +32,7 @@ export const MainDiv = styled.div`
   align-items: center;
   padding-top: 6rem;
   gap: 5rem;
+
   @media only screen and (max-width: 767px) {
     padding-top: 4rem;
     gap: 3rem;
@@ -34,6 +45,11 @@ export const ButtonDiv = styled.div`
   padding-right: 20rem;
   display: flex;
   justify-content: flex-end;
+
+  @media only screen and (max-width: 1199px) {
+    padding-right: 5rem;
+  }
+
   @media only screen and (max-width: 767px) {
     justify-content: center;
     padding-right: 0;
@@ -44,7 +60,9 @@ export const ButtonDiv = styled.div`
 export const LogoImg = styled.img`
   width: 46rem;
   padding-top: 4rem;
+
   @media only screen and (max-width: 767px) {
+    padding-top: 0;
     width: 25rem;
     order: 1;
   }
@@ -67,6 +85,7 @@ export const Inputdiv = styled.form`
   gap: 1.6rem;
   background-color: var(--Grayscale-10, #fff);
   border-radius: 1.6rem;
+
   @media only screen and (max-width: 767px) {
     width: 30rem;
     height: 22rem;
@@ -89,18 +108,25 @@ export const LoginText = styled.p`
   font-size: 1.6rem;
 `
 
-export const MobileImgDiv = styled.div`
+export const ResponsiveImgDiv = styled.div`
   width: 100%;
   display: flex;
   justify-content: center;
   align-items: center;
 `
 
-export const MobileImg = styled.img`
+export const ResponsiveImg = styled.img`
   display: none;
+
+  @media only screen and (max-width: 1199px) {
+    display: block;
+    width: 100%;
+    height: 100%;
+  }
+
   @media only screen and (max-width: 767px) {
     display: block;
-    width: 37.5rem;
-    height: 17rem;
+    width: 100%;
+    height: 100%;
   }
 `
