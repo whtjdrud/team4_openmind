d44d6fcbeea9a0c7b23b3bf81b666113d1bdd726
Jo hyeonjin <guswlso2@naver.com>
2024-01-29 21:05:43 +0900

    fix: list초기값 time으로 변경

--- src/components/atomicComponents/QuestionModal/index.jsx
+++ src/components/atomicComponents/QuestionModal/index.jsx
@@ -1,13 +1,29 @@
-import React, { useRef } from 'react'
+import React, { useEffect, useRef } from 'react'
 import { StyledModalContent, StyledOverlay } from './StyledQuestionModal'
 import MESSAGE from '../../../assets/images/Messages.svg'
 import CLOSE from '../../../assets/images/Close.svg'
 import FeedCardAnswer from '../FeedCardQuestion'
 
 const QuestionModal = ({ closeModal, name, image, id }) => {
+  const modalRef = useRef()
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (modalRef.current && !modalRef.current.contains(event.target)) {
+        closeModal()
+      }
+    }
+
+    document.addEventListener('mousedown', handleClickOutside)
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside)
+    }
+  }, [modalRef, closeModal])
+
   return (
     <StyledOverlay>
-      <StyledModalContent>
+      <StyledModalContent ref={modalRef}>
         <div className='modal-header'>
           <div className='title-box'>
             <img alt='메시지' src={MESSAGE} />
